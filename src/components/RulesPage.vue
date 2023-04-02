7747d97ff205364a309aead399b6b784089741af
larsmorow <maxmudov731@gmail.com>
2023-04-02 17:24:49 +0300

    all header

--- src/components/RulesPage.vue
+++ src/components/RulesPage.vue
@@ -1,6 +1,18 @@
 <template>
 	<div class="wrapper">
-	
+<header>
+<nav>
+	<ul>
+		<li><router-link to="/home">Главная</router-link></li>
+		<li><router-link to="/about">О нас</router-link></li>
+		<li><router-link to="/contact">Контакты</router-link></li>
+		<li><router-link to="/faq">FAQ</router-link></li>
+		<li><router-link to="/support">Поддержка</router-link></li>
+		<li><router-link to="/rules">Правила магазина</router-link></li>
+		<li><router-link to="/shop">Магазин</router-link></li>
+	</ul>
+</nav>
+</header>
 
 	</div>
 </template>
